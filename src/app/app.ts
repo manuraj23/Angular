6f5ffd674d2f1a175c7635a420c4a3a1c4efd72d
manuraj23 <manuraj082004@gmail.com>
2026-01-12 22:22:10 +0530

    Counter App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Signup } from './signup/signup';
 import { Profile } from './profile/profile';
+import { Counter } from './counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup,Profile],
+  imports: [Login, Signup,Profile,Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-app-angular');
+  click_Function() {
+    alert("Button Clicked - Going to Login Page");
+  }
 }
---
+++ src/app/counter/counter.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter {
+  count: number = 0;
+  
+
+  //M1: Methods to modify the count
+  incrementCount() {
+    this.count++;
+  }
+  decrementCount() {
+    this.count--;
+  }
+  
+  resetCount() {
+    this.count = 0;
+  }
+  //M2: Method to modify counter;
+  handleCounter(action: string) {
+    switch (action) {
+      case 'increment':
+        this.count++;
+        break;
+      case 'decrement':
+        if (this.count > 0){
+          this.count--;
+        }
+        else{
+          alert("Count cannot be negative");
+        }
+        break;
+      case 'reset':
+        this.count = 0;
+        break;
+      default:
+        break;
+    }
+  }
+
+}
